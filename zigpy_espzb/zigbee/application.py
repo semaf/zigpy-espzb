881cd468bedb9b440d69398eb7a3d5fa9b58fc45
semaf electronics <semaf@users.noreply.github.com>
2025-05-13 18:38:38 +0200

    Fixes by @puddly

--- zigpy_espzb/zigbee/application.py
+++ zigpy_espzb/zigbee/application.py
@@ -52,7 +52,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
     def __init__(self, config: dict[str, Any]):
         """Initialize instance."""
 
-        super().__init__(config=zigpy.config.ZIGPY_SCHEMA(config))
+        super().__init__(config=config)
         self._api = None
 
         self._pending = zigpy.util.Requests()
@@ -329,7 +329,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         if t.TransmitOptions.APS_Encryption in packet.tx_options:
             tx_options |= TransmitOptions.SECURITY_ENABLED
 
-        async with self._limit_concurrency():
+        async with self._limit_concurrency(priority=packet.priority):
             await self._api.aps_data_request(
                 dst_addr=dst_addr,
                 dst_ep=packet.dst_ep,
